e8478e318208c201787b27e129ba1931f10020c0
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2022-08-15 13:00:22 +0100

    changed the database to postgresql

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 1
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'emerald-todo.herokuapp.com']
 
@@ -107,11 +107,14 @@ WSGI_APPLICATION = 'TaskManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -181,7 +184,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-DEFAULT_FROM_EMAIL ='emerald<noreply@emerald-todo.herokuapp.com>'
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 #SERVER_MAIL = 'usman<@localhost>'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
